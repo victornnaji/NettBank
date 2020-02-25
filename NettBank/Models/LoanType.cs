af1fddb01ddd737ccdcfa685993634db1b8dc742
Nnaji Victor <nnajivictor0@gmail.com>
2020-02-25 13:35:59 +0100

    restructuring code

---
+++ NettBank/Models/LoanType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NettBank.Models
+{
+    public class LoanType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
